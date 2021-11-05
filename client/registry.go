6ba6e30f0c8b4f0c12bdcf16972840edab1f30a8
Simon Fels <simon.fels@canonical.com>
2021-11-05 15:53:28 +0100

    Upload 1.11.2 SDK

--- client/registry.go
+++ client/registry.go
@@ -1,6 +1,20 @@
 // -*- Mode: Go; indent-tabs-mode: t -*-
-// AMS - Anbox Management Service
-// Copyright 2020 Canonical Ltd.  All rights reserved.
+/*
+ * This file is part of AMS SDK
+ * Copyright 2021 Canonical Ltd.
+ *
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License version 3, as published
+ * by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranties of MERCHANTABILITY, SATISFACTORY
+ * QUALITY, or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public
+ * License for more details.
+ *
+ * You should have received a copy of the Lesser GNU General Public License along
+ * with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 package client
 
--- go.mod
+++
@@ -1,9 +0,0 @@
-module github.com/anbox-cloud/ams-sdk
-
-go 1.12
-
-require (
-	github.com/gorilla/websocket v1.4.2
-	golang.org/x/crypto v0.0.0-20201124201722-c8d3bf9c5392
-	gopkg.in/yaml.v2 v2.4.0
-)
